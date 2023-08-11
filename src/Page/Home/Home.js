a897074b4615def3e532cd1b1e5746747463b234
Abdelrahman0000 <khlyftbdh5@gmail.com>
2023-08-11 02:42:55 +0200

    new sections

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,7 +1,75 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import './home.css';
+
+import Hbg from "../../image/bg/h1.jpg";
+
+import Hbg1 from "../../image/bg/h2.jpg";
+// import required modules
+import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
 
 export default function Home() {
   return (
-    <h1>Welocom home baby</h1>
+    <div className="Home">
+  <Swiper
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+
+<div className=" box">
+  <img src={Hbg} alt="" />
+  <div className="box-inner">
+<h4>T-shirt / Tops</h4>
+<h1>
+Summer 
+Value Pack
+</h1>
+<h4>
+cool / colorful / comfy
+</h4>
+<div className="above">
+  <button className="my-btn" >
+  Shop Now
+  </button>
+</div>
+  </div>
+</div>
+
+        </SwiperSlide>
+        <SwiperSlide>
+
+<div className=" box">
+  <img src={Hbg1} alt="" />
+  <div className="box-inner">
+<h4>T-shirt / Tops</h4>
+<h1>
+all brands here
+</h1>
+<h4>
+cool / colorful / great
+</h4>
+<div className="above">
+  <button className="my-btn" >
+  Shop Now
+  </button>
+</div>
+  </div>
+</div>
+
+        </SwiperSlide>
+      </Swiper>
+    </div>
   )
 }
